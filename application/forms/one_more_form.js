979fc7eea1b9810b11658f35081dddb175465dec
goodhousekeeper <github@goodhousekeeper.ru>
2019-07-24 19:41:08 +0300

    Create another form

---
+++ application/forms/one_more_form.js
@@ -0,0 +1,14 @@
+let oneMoreFormProperties = {
+    name:'oneMoreForm',
+    className:"TForm",
+    caption:"Delphi.js Boilerplate - One More Form",
+    width:700,
+    height:400,
+    screenCenter:true,
+    visible:true,
+    icon:"application/resources/images/icons/radioactivity.png",
+    content:{}
+
+}
+
+export default oneMoreFormProperties;
