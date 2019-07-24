4bf355c4420e000c7d26716ed8c2110a2f63371f
goodhousekeeper <github@goodhousekeeper.ru>
2019-07-24 19:40:57 +0300

    Create another form

--- application/init.js
+++ application/init.js
@@ -6,10 +6,13 @@ app.caption = 'New project on Delphi.js';
 /* Add custom components */
 
 /* get forms definition */
-import mainFormProperties from  './forms/main_form.js'
+import mainFormProperties from  './forms/main_form.js';
+import oneMoreFormProperties from  './forms/one_more_form.js'
 
 
-let form = app.createObject(mainFormProperties);
+let mainForm = app.createObject(mainFormProperties);
+let oneMoreForm = app.createObject(oneMoreFormProperties);
 
-console.info(app.mainForm)
+console.info(app.mainForm);
+console.info(app.oneMoreForm);
 
