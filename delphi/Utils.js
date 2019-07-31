854d81707b0479cd9e5bd82f2540968c7adbc247
goodhousekeeper <github@goodhousekeeper.ru>
2019-07-31 17:00:54 +0300

    misspell

--- delphi/Utils.js
+++ delphi/Utils.js
@@ -38,7 +38,7 @@ export default class Utils {
         const timing = options.timing ? options.timing : Constants.ANIMATION_FUNCTION_LINEAR;
 
         window.requestAnimationFrame(function animate (time) {
-            /* timeFraction от 0 до 1 */
+            /* timeFraction from 0 to 1 */
             let timeFraction = (time - start) / duration;
             if (timeFraction > 1) {
                 timeFraction = 1;
