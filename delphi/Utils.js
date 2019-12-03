05e72878567960d58bfbef93521e0a742c40b8ef
goodhousekeeper <github@goodhousekeeper.ru>
2019-12-03 19:03:56 +0300

    Utils.js - add semicolons

--- delphi/Utils.js
+++ delphi/Utils.js
@@ -9,68 +9,68 @@ export default class Utils {
      * @param {string} style - css styles in text format
      */
   static addStyleNode (style) {
-    const styleNode = document.createElement('style')
-    styleNode.title = Constants.STYLE_TAG_TITLE
-    styleNode.innerHTML = style.replace(/\s+/g, ' ')
-    document.getElementsByTagName('head')[0].appendChild(styleNode)
+    const styleNode = document.createElement('style');
+    styleNode.title = Constants.STYLE_TAG_TITLE;
+    styleNode.innerHTML = style.replace(/\s+/g, ' ');
+    document.getElementsByTagName('head')[0].appendChild(styleNode);
   }
 
   static updateStyleNode (style) {
-    let styleNode
+    let styleNode;
     for (const node of document.getElementsByTagName('style')) {
       if (node.title === Constants.STYLE_TAG_TITLE) {
-        styleNode = node
+        styleNode = node;
       }
     }
     if (!styleNode) {
-      return
+      return;
     }
-    styleNode.innerHTML += style.replace(/\s+/g, ' ')
+    styleNode.innerHTML += style.replace(/\s+/g, ' ');
   }
 
   static addFaviconNode (favicon) {
-    const headNode = document.getElementsByTagName('head')[0]
-    let iconNode
+    const headNode = document.getElementsByTagName('head')[0];
+    let iconNode;
     headNode.childNodes.forEach((item) => {
       if (item.rel === 'shortcut icon') {
-        iconNode = item
+        iconNode = item;
       }
-    })
+    });
     if (!iconNode) {
-      iconNode = document.createElement('link')
-      iconNode.rel = 'shortcut icon'
-      document.getElementsByTagName('head')[0].appendChild(iconNode)
+      iconNode = document.createElement('link');
+      iconNode.rel = 'shortcut icon';
+      document.getElementsByTagName('head')[0].appendChild(iconNode);
     }
-    iconNode.href = favicon
+    iconNode.href = favicon;
   }
 
   static animate (options) {
-    const start = performance.now()
+    const start = performance.now();
 
-    const duration = options.duration || options.duration === 0 ? options.duration : Constants.ANIMATION_SPEED
-    const timing = options.timing ? options.timing : Constants.ANIMATION_FUNCTION_LINEAR
+    const duration = options.duration || options.duration === 0 ? options.duration : Constants.ANIMATION_SPEED;
+    const timing = options.timing ? options.timing : Constants.ANIMATION_FUNCTION_LINEAR;
 
     window.requestAnimationFrame(function animate (time) {
       /* timeFraction from 0 to 1 */
-      let timeFraction = (time - start) / duration
+      let timeFraction = (time - start) / duration;
       if (timeFraction > 1) {
-        timeFraction = 1
+        timeFraction = 1;
       }
       /* current animation state */
-      const progress = timing(timeFraction)
-      options.draw(progress)
+      const progress = timing(timeFraction);
+      options.draw(progress);
       if (timeFraction < 1) {
-        window.requestAnimationFrame(animate)
+        window.requestAnimationFrame(animate);
       } else {
         if (options.callback) {
-          options.callback()
+          options.callback();
         }
       }
     })
   }
 
   static getAbsolutePathToResource(resourcePath) {
-    const location = window.location.href
-    return `${location.replace('/index.html','')}/${resourcePath}`
+    const location = window.location.href;
+    return `${location.replace('/index.html','')}/${resourcePath}`;
   }
 }
