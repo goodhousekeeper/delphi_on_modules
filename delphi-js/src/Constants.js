7be8831a3f8726ffd819dc7803e00357799acc79
goodhousekeeper <github@goodhousekeeper.ru>
2019-12-17 19:07:50 +0300

    app styles in css variables

--- delphi-js/src/Constants.js
+++ delphi-js/src/Constants.js
@@ -25,6 +25,11 @@ html {
     user-select: none;
     overflow: auto;
     
+    --background-color: #E6E6E6; 
+    --background-image: url('data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQAQMAAAC6caSPAAAAAXNSR0IArs4c6QAAAAZQTFRF////zsrFFOhhzwAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfbCA8IIAxOHyTqAAAAbElEQVR42u3PMQ0AAAgDsMlHNhqWcHC0Dpqp5S0XFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXF5cjC3IDYNdXepeXAAAAAElFTkSuQmCC');
+    --background-image-size: auto;   
+    --background-image-repeat: repeat;
+    --background-image-position: 0% 0%;  
     --font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif; 
     --font-size: 14px;
     --font-color: #1F2933;
@@ -53,10 +58,11 @@ body {
     height: 100%;
     width: 100%;
     overflow: hidden;
-    background-color: #E6E6E6;
-    background-image: url('data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQAQMAAAC6caSPAAAAAXNSR0IArs4c6QAAAAZQTFRF////zsrFFOhhzwAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfbCA8IIAxOHyTqAAAAbElEQVR42u3PMQ0AAAgDsMlHNhqWcHC0Dpqp5S0XFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXFxcXF5cjC3IDYNdXepeXAAAAAElFTkSuQmCC');
-    background-size: auto;
-    background-repeat: repeat;
+    background-color: var(--background-color);
+    background-image: var(--background-image);
+    background-size: var(--background-image-size);
+    background-repeat: var(--background-image-repeat);
+    background-position: var(--background-image-position);
     font-family: var(--font-family);
     font-size: var(--font-size);
 }
